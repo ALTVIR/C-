b6c4a18922374c4bd7c2fd8d55bd40fb5299baad
ALTAIR <altair.kabdushev@gmail.com>
2023-03-03 12:21:21 +0600

    Homework for Seminar1 ended

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -58,3 +58,96 @@ while(current <= num)
 // 31542 % 10000 = 1542
 
 // 453216 -> 32
+
+// HOMEWORK
+
+// Task 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+/*
+Console.Write("Input a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max, min;
+
+if (a > b)
+{   max = a;
+    min = b;;
+}
+else
+{
+    max = b;
+    min = a;
+}
+
+Console.WriteLine("Higher number: {0}", max);
+Console.WriteLine("Lower number: {0}", min);
+*/
+
+// Task 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+/*
+Console.Write("Input a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max;
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+ if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine("Max number: {0}", max);
+*/
+
+// Task 6. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+/*
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("number {0} even", num);
+}
+else
+{
+    Console.WriteLine("number {0} odd", num);
+}
+*/
+
+// Task 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+/*
+Console.Write("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Even numbers from 1 to {0}: ", n);
+
+for (int i = 2; i <= n; i += 2)
+{
+    Console.Write("{0} ", i);
+}
+*/
